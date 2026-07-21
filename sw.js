7ec47bbfe4de1aeb8c55710223a1b67ef1588c22
crava8474-byte <crava8474@gmail.com>
2026-07-22 02:53:56 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='crava-realistic-box-v1';
-const ASSETS=['./','./index.html','./config.js','./crava-logo-clean.png','./brand-wordmark.png','./icon-192.png','./icon-512.png','./manifest.webmanifest'];
+const CACHE='crava-production-box-v1';
+const ASSETS=['./','./index.html','./config.js','./crava-logo-clean.png','./brand-wordmark.png','./box-pattern.svg','./icon-192.png','./icon-512.png','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
